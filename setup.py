8bbb0029b436fd4d4010fef0ff552c23a36ff595
Jhengsh <jhengsh.email@gmail.com>
2018-05-13 01:40:15 +0800

    add string function

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tidyframe',
-    version='0.0.2dev',
+    version='0.0.1',
     author='Hsueh-Hung Cheng',
     author_email='jhengsh.email@gmail.com',
     packages=find_packages(),
--- tidyframe/tools/__init__.py
+++ tidyframe/tools/__init__.py
@@ -1,5 +1,6 @@
 from .select import select
 from .separate import separate
 from .combination import combination
+from .string import replace_by_dict
 
-__all__ = ['select', 'separate', 'combination']
+__all__ = ['select', 'separate', 'combination', 'replace_by_dict']

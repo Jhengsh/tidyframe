238b2d27966998cc07506fa3921ee019cce45e8f
Jhengsh <jhengsh.email@gmail.com>
2018-07-05 00:33:37 +0800

    add select parameter

--- tidyframe/tools/select.py
+++ tidyframe/tools/select.py
@@ -8,33 +8,49 @@ def select(df,
            columns=None,
            columns_minus=None,
            columns_between=None,
+           pattern=None,
            copy=False):
     """
-    Select Pandas DataFrame by minus
+    Select Pandas DataFrame Columns
 
     Parameters
     ----------
     df : Pandas DataFrame
     columns_minus : column which want to remove
+    columns_between: list with two element, select columns bwtween two columns
+    pattern: regular expression or list of regular expression, return match columns
     copy : whether return deep copy DataFrame
 
     Returns
     -------
     df_return : Pandas DataFrame
     """
-    if columns is not None:
+    if columns:
         df_return = df[columns]
-    if columns_minus is not None:
+    if columns_minus:
         raw_col = {value: i for i, value in enumerate(df.columns)}
         for pop_key in columns_minus:
             raw_col.pop(pop_key)
         df_return = df[list(raw_col.keys())]
-    if columns_between is not None:
+    if columns_between:
         columns_location = {column: i for i, column in enumerate(df.columns)}
         assert columns_location[columns_between[0]] < columns_location[columns_between[1]], 'first column location must less than second column location'
         df_return = df.iloc[:,
                             range(columns_location[columns_between[0]],
                                   columns_location[columns_between[1]] + 1)]
+    if pattern and isinstance(pattern, str):
+        columns_want = list(
+            filter(lambda x: re.search(pattern, x), df.columns))
+        df_return = df[columns_want]
+    if pattern and isinstance(pattern, list):
+        columns_want = []
+        for each_pattern in pattern:
+            columns_want.extend(
+                list(filter(lambda x: re.search(each_pattern, x), df.columns)))
+            columns_want = list(set(columns_want))
+            columns_want.sort()
+            df_return = df[columns_want]
+
     if copy:
         return deepcopy(df_return)
     else:
